3fed30a0b04092e0a22ae61d2ad77c4a4a543e1b
JEXx919 <ibarravaldezjesus99@gmail.com>
2024-11-13 23:15:24 -0600

    act

---
+++ p145_EstudianteGUI_V2/Util.java
@@ -0,0 +1,36 @@
+package p145_EstudianteGUI_V2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import p101_TrabajoPersona.Persona;
+
+public class Util {
+    public static ArrayList<Estudiante> inicializarDatos() {
+        ArrayList<Estudiante> datos = new ArrayList<>();
+        datos.add(new Estudiante("Juan perez", 22,34.5,"jperez@gmail.com"));
+        datos.add(new Estudiante("Maria Jimenez", 18,22.5,"mjimenez@gmail.com"));
+        datos.add(new Estudiante("Carlos Fuentes", 30,45,"cfuentes@gmail.com"));
+        return datos;
+
+    }
+
+    public static void grabarDatos(String archivo, ArrayList<Estudiante> datos ) throws IOException{
+        ObjectOutputStream fdatos = new ObjectOutputStream(new FileOutputStream(archivo));
+        fdatos.writeObject(datos);
+        fdatos.close();
+    }
+
+    public static ArrayList<Estudiante> leerDatos(String archivo) throws IOException, ClassNotFoundException{
+        ArrayList<Estudiante> datos = new ArrayList<>();
+        ObjectInputStream fdatos = new ObjectInputStream(new FileInputStream(archivo));
+        datos = (ArrayList<Estudiante>) fdatos.readObject();
+        fdatos.close();
+        return datos;
+    }
+
+}
